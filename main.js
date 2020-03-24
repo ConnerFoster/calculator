499e6056092bf7064d2ca73304eaeb7ffc6a53b6
Conner Foster <connerf99@gmail.com>
2020-03-23 21:36:29 -0400

    some basic javascript written

---
+++ main.js
@@ -0,0 +1,43 @@
+const clearButton = document.getElementById('clear');
+const squaredButton = document.getElementById('squared');
+const moduloButton = document.getElementById('modulo');
+const divideButton = document.getElementById('divide');
+const multiplyButton = document.getElementById('multiply');
+const subtractButton = document.getElementById('subtract');
+const addButton = document.getElementById('add');
+const equalsButton = document.getElementById('equals');
+const oneButton = document.getElementById('one');
+const twoButton = document.getElementById('two');
+const threeButton = document.getElementById('three');
+const fourButton = document.getElementById('four');
+const fiveButton = document.getElementById('five');
+const sixButton = document.getElementById('six');
+const sevenButton = document.getElementById('seven');
+const eightButton = document.getElementById('eight');
+const nineButton = document.getElementById('nine');
+const zeroButton = document.getElementById('zero');
+const decimalButton = document.getElementById('decimal');
+const buttonDiv = document.getElementById('button-div');
+const display = document.getElementById('display');
+const calculatorDiv = document.getElementById('calculator');
+
+function add(num1,num2){
+    return num1 + num2;
+}
+function subtract(num1,num2){
+    return num1 - num2;
+}
+function multiply(num1,num2){
+    return num1 * num2;
+}
+function divide(num1,num2){
+    return num1 / num2;
+}
+function modulo(num1,num2){
+    return num1 % num2;
+}
+function squared(num){
+    return num*num;
+}
+
+
