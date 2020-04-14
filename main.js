f6dd8d9f8be17b51769af5bdbfb122c477c13da7
Conner Foster <connerf99@gmail.com>
2020-04-13 20:49:57 -0400

    most of the calculator is working, still a few things to modify

--- main.js
+++ main.js
@@ -20,77 +20,128 @@ const decimalButton = document.getElementById('decimal');
 const buttonDiv = document.getElementById('button-div');
 const display = document.getElementById('display');
 const calculatorDiv = document.getElementById('calculator');
-
+const displayText = document.getElementById('displayText');
 let displayArray = [];
+oneButton.addEventListener('click', displayButtons);
+twoButton.addEventListener('click', displayButtons);
+threeButton.addEventListener('click', displayButtons);
+fourButton.addEventListener('click', displayButtons);
+fiveButton.addEventListener('click', displayButtons);
+sixButton.addEventListener('click', displayButtons);
+sevenButton.addEventListener('click', displayButtons);
+eightButton.addEventListener('click', displayButtons);
+nineButton.addEventListener('click', displayButtons);
+zeroButton.addEventListener('click', displayButtons)
+decimalButton.addEventListener('click', displayDecimal)
+divideButton.addEventListener('click', operatorPressed);
+multiplyButton.addEventListener('click', operatorPressed);
+addButton.addEventListener('click', operatorPressed);
+subtractButton.addEventListener('click', operatorPressed);
+clearButton.addEventListener('click', reset);
+equalsButton.addEventListener('click', equalPressed)
 
-
-function add(num1,num2){
-    display.textContent = num1 + num2;
-}
-function subtract(num1,num2){
-    return num1 - num2;
-}
-function multiply(num1,num2){
-    return num1 * num2;
-}
-function divide(num1,num2){
-    return num1 / num2;
-}
-function modulo(num1,num2){
-    return num1 % num2;
-}
-function squared(num){
-    return num*num;
-}
-
-//function operate() {
-    //const operators = ["add","subtract","multiply","divide"];
-
-
-//}
-function display1() {
-    displayArray.push(1);
-    display.textContent = displayArray.join(" ");
-    console.log(displayArray.toString());
-}
-function display2() {
-    displayArray.push(2);
-    display.textContent = displayArray.join(" ");
-    console.log(displayArray.toString());
-}
-/*let num1 = 0;
+let num1 = 0;
 let num2 = 0;
-function storeNum1() {
-    num1 = displayArray.join("");
-    displayArray.length = 0;
-    display.textContent = " ";
+let operator = '';
+let ans = 0;
+let lastChar = displayArray[displayArray.length-1];
+
+
+
+
+function add(num1, num2) {
+  return Number(num1) + Number(num2);
+  
 }
-function storeNum2() {
-    num2 = displayArray.join("");
-    displayArray.length = 0;
-    display.textContent = " ";
-}*/
-function operate(operator,num1,num2) {
-    if (operator == "addition") {
-        add(num1,num2);
-    }
-    else if (operator == "subtraction") {
-        subtract(num1,num2);
-    }
-    else if (operator == "multiplication") {
-        multiply(num1,num2);
-    }
-    else if (operator == "division") {
-        divide(num1,num2);
-    }
-    else {
-        console.log('error')
-    }
+
+function subtract(num1, num2) {
+  return num1 - num2;
+  
 }
-oneButton.addEventListener('click', display1);
-twoButton.addEventListener('click', display2);
-addButton.addEventListener('click', operate);
+
+function multiply(num1, num2) {
+  return num1 * num2;
+  
+}
+
+function divide(num1, num2) {
+  return num1 / num2;
+  
+}
+
+function modulo(num1, num2) {
+  return num1 % num2;
+}
+
+function squared(num) {
+  return num * num;
+}
+
+function displayButtons(evt) {
+  displayArray.push(evt.target.value);
+  displayText.textContent = displayArray.join('')
+}
+function displayDecimal() {
+    if(displayArray.includes('.')){
+        displayText.textContent = displayArray.join('');
+    }
+    else{
+        displayArray.push('.');
+        displayText.textContent = displayArray.join('')
+    }
+    
+}
+
+function operatorPressed(evt) {
+  if(operator == false) {
+  num1 = displayArray.join('');
+  displayArray.length = 0;
+  operator = evt.target.value;
+  }
+  else {
+  operator = evt.target.value;
+  num1 = ans;
+  }
+  
+}
+
+function equalPressed() {
+  num2 = displayArray.join('');
+  operate(operator, num1, num2);
+  displayArray.length = 0;
+  
+}
+
+function operate(operator, num1, num2) {
+  if (operator == "add") {
+    displayText.textContent = add(num1, num2);
+    operator ='';
+    ans = add(num1,num2);
+    
+  } else if (operator == "subtract") {
+    displayText.textContent = subtract(num1, num2);
+    operator = '';
+    ans = subtract(num1,num2);
+    
+  } else if (operator == "multiply") {
+    displayText.textContent = multiply(num1, num2);
+    operator = '';
+    ans = multiply(num1,num2);
+  } else if (operator == "divide") {
+
+    displayText.textContent = divide(num1, num2);
+    operator = '';
+    ans = divide(num1,num2);
+
+  } else {
+    console.log('error')
+  }
+}
 
 
 
-
+function reset() {
+  location.reload();
+  //displayText.textContent = '0';
+  //displayArray.length = 0;
+}
